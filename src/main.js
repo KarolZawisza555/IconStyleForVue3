a078570b985010501ec6c4d7bd2600c69f6f38f7
Karol Zawisza <118629530+KarolZawisza555@users.noreply.github.com>
2023-03-11 22:48:11 +0100

    changen order in main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 import { Icon } from '@iconify/vue';
 import Vue3ColorPicker from "vue3-colorpicker";
 import "vue3-colorpicker/style.css";
